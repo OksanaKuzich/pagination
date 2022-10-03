3b63fca8ac5530311f6e1983c35c9b9ef40b9c8b
OksanaKuzich <komoks255@gmail.com>
2022-10-03 20:37:24 +0300

    start

---
+++ src/fetchImages.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { Notify } from 'notiflix';
+
+export function fetchImages(searchQuery, page) {
+  const searchParams = new URLSearchParams({
+    key: '30075505-2d85101d237fc7d98fa57be1d',
+    q: searchQuery,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: page,
+    per_page: 40,
+  });
+
+  const URL = `https://pixabay.com/api/?${searchParams}`;
+
+  return axios
+    .get(URL)
+    .then(res => res.data)
+    .catch(console.log);
+}
